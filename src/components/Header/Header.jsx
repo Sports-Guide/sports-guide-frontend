c02e574f862fa6b801865049fdcfe38a7502e5df
AndreiZhura <zhura2296@yandex.ru>
2023-12-23 18:07:40 +0300

    app.scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
 import { Button } from '../Button/Button';
+import logo from '../../images/logo.svg';
 import './Header.scss';
 
 export function Header({ onLogIn, onLogOut, loggedIn }) {
 	return (
 		<header className="header">
+			<div className="header__container">
+				<img className="logo" src={logo} alt="" />
+			</div>
 			{loggedIn ? (
 				<Button onClick={onLogOut} label="Выйти" />
 			) : (
