305a104bf6a33a12d566f8ba0913a0161c18a6df
AndreiZhura <zhura2296@yandex.ru>
2023-12-23 12:04:42 +0300

    sass

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import './Footer.scss';
 export function Footer() {
 	return (
 		<footer className="footer">
-			<h1>Всем Привет</h1>
+			<h1>Всем Приветпше</h1>
+			<p> Пробуем изменения</p>
 			<p className="footer__copyright">© 2022</p>
 		</footer>
 	);
