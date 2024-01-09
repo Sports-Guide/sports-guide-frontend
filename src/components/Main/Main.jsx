ae9ccfdf6820efc40084bb01520ba47ce0c6e3fe
Sasha Ivanova <alexskhnv@yandex.ru>
2024-01-10 00:29:02 +0300

    feat(marker): add marker on click

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,25 @@
+import React from 'react';
 import './Main.scss';
+import { Marker } from 'react-leaflet';
+// import PropTypes from 'prop-types';
 import { Form } from '../Form/Form';
 import MapComponent from '../Map/Map';
 import { SearchControl } from '../SearchControl/SearchControl';
+import customIcon from '../CustomIcon/CustomIcon';
+import MarkerLayer from '../MarkerLayer/MarkerLayer';
 
 export function Main() {
+	const [newMarkers, setNewMarkers] = React.useState([]);
+
 	return (
 		<main>
 			<Form label="Найти" />
 			<MapComponent className="map-container_place_main">
 				<SearchControl />
+				<MarkerLayer newwMarkers={newMarkers} setNewMarkers={setNewMarkers} />
+				{newMarkers.map((marker) => (
+					<Marker position={marker} icon={customIcon} draggable />
+				))}
 			</MapComponent>
 		</main>
 	);
