5def9bb2931715bf4bd452b958209f4c1175d80f
AndreiZhura <zhura2296@yandex.ru>
2024-01-11 17:11:02 +0300

    style(foto input): 1
    1

--- src/components/Area/AreaApp.jsx
+++ src/components/Area/AreaApp.jsx
@@ -3,8 +3,10 @@ import './AreaApp.scss';
 import { NavLink } from 'react-router-dom';
 import { ButtonKindsOfSports } from '../Button/ButtonKindsOfSports';
 import MapComponent from '../Map/Map';
+import addPictures from '../../images/icon-plus.svg';
+import { ButtonAppSite } from '../Button/ButtonAppSite';
 
-export function AreaApp() {
+export function AreaApp(onAreaApp) {
 	return (
 		<div className="area-app">
 			<div className="area-app__information">
@@ -35,6 +37,17 @@ export function AreaApp() {
 					<MapComponent className="map-container_place_area-app" />
 					<div className="kinds-of-sports">
 						<h3 className="kinds-of-sports__title">Виды спорта</h3>
+						<form className="input-container">
+							<label htmlFor="name-site" className="input-container__label">
+								Название площадки*
+								<input
+									type="text"
+									className="input-container__input"
+									name="name-site"
+									placeholder="Например, Футбольное поле"
+								/>
+							</label>
+						</form>
 						<div className="kinds-of-sports__buttons">
 							<ButtonKindsOfSports ball="football" label="Футбол" />
 							<ButtonKindsOfSports ball="bascetball" label="Баскетбол" />
@@ -43,6 +56,27 @@ export function AreaApp() {
 							<ButtonKindsOfSports ball="bascetball" label="Тренажеры" />
 						</div>
 					</div>
+					<div className="foto">
+						<h3 className="foto__title">Фотографии</h3>
+						<div className="foto__container">
+							<label htmlFor="add-file" className="foto__file-label">
+								<input
+									type="file"
+									className="foto__file-add"
+									name="add-file"
+									multiple
+									accept="image/png, image/jpeg"
+									id="add-file"
+								/>
+								<img
+									className="foto-file-img"
+									src={addPictures}
+									alt="Добавление картинки"
+								/>
+							</label>
+						</div>
+					</div>
+					<ButtonAppSite onClick={onAreaApp} label="Добавить площадку" />
 				</form>
 			</div>
 		</div>
