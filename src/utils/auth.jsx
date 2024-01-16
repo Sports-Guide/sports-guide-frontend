c2579440aa566cae4bb73d394be58096ac44aad7
Elena Kainarova <helenrizik@yandex.ru>
2024-01-16 22:34:20 +0300

    refactor(add auth): add auth

---
+++ src/utils/auth.jsx
@@ -0,0 +1,50 @@
+const BASE_URL = 'https://sports-map.ru/api/auth';
+
+/* const BASE_URL = "http://localhost:3000";
+https://sports-map.ru/api/auth/jwt/create/
+https://sports-map.ru/api/auth/jwt/refresh/
+https://sports-map.ru/api/auth/jwt/verify/ 
+https://sports-map.ru/api/auth/users/
+*/
+
+const checkResponse = (res) => {
+	if (res.ok) {
+		return res.json();
+	}
+	return Promise.reject(res.status);
+};
+
+// Регистрация пользователя
+function register(nickname, email, password) {
+	return fetch(`${BASE_URL}/signup`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(nickname, email, password),
+	}).then(checkResponse);
+}
+
+// Авторизация пользователя
+function login(email, password) {
+	return fetch(`${BASE_URL}/signin`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(email, password),
+	}).then(checkResponse);
+}
+
+// Аутинфикация пользователя
+function checkToken(token) {
+	return fetch(`${BASE_URL}/users/me`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`,
+		},
+	}).then(checkResponse);
+}
+
+export { register, login, checkToken };
