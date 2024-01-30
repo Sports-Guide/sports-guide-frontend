a826ed17dd4034def055d0b260446b2b2a0416ae
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-01-30 20:55:42 +0200

    add register user in redux

--- src/services/store.js
+++ src/services/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authUserSlise from './slices/userSlice';
+import registerUserSlise from './slices/registerUserSliсe';
 
 const rootReducer = combineReducers({
 	authUser: authUserSlise,
+	registerUser: registerUserSlise,
 });
 
 export const store = configureStore({
