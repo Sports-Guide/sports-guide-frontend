d8b9db16ea346b59c648d130f18376f254fd759b
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-02-29 20:27:18 +0200

    delete trash

--- src/components/CustomIcon/CustomIcon.jsx
+++
@@ -1,9 +0,0 @@
-import { Icon } from 'leaflet';
-import customIconImage from '../../images/location.png';
-
-const customIcon = new Icon({
-	iconUrl: customIconImage,
-	iconSize: [38, 38],
-});
-
-export default customIcon;
