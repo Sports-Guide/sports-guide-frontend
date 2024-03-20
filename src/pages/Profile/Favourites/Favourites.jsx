0247664905aeb0fca344ecd2c598b7eff706060e
Victoria Matynyan <victoria.matynyan@gmail.com>
2024-03-20 14:46:35 +0300

    feature: prepare Favourites page, set switching rendering between cards and list

--- src/pages/Profile/Favourites/Favourites.jsx
+++ src/pages/Profile/Favourites/Favourites.jsx
@@ -1,24 +1,66 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-// import { openModal } from '../../../services/slices/modalSlice';
-import { fetchUserInfo } from '../../../services/thunks/userThunk';
-// import { getUserInfo } from '../../../services/selectors/userSelector';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Card from '../../../components/Card/Card';
+import { fetchGetFavoriteAreas } from '../../../services/thunks/favoriteAreasThunk';
+import { favoriteAreaList } from '../../../services/selectors/areaSelector';
 
 import './Favourites.scss';
 
 export default function Favourites() {
 	const dispatch = useDispatch();
-	// const user = useSelector(getUserInfo);
+	const favoritesArea = useSelector(favoriteAreaList);
+	const [isCardRendered, setIsCardRendered] = useState(true);
 
 	useEffect(() => {
-		dispatch(fetchUserInfo());
+		dispatch(fetchGetFavoriteAreas());
 	}, [dispatch]);
+
+	const handleSwitchCardsDisplay = () => {
+		if (isCardRendered) {
+			setIsCardRendered(false);
+		} else setIsCardRendered(true);
+	};
 
 	return (
 		<section className="favourites">
 			<nav className="favourites__navigation">
-				<h2 className="form__title_place_profile">Избранные площадки</h2>
+				<h2 className="favourites__title">Избранные площадки</h2>
+				<div className="favourites__buttons">
+					<button
+						className={`favourites__button ${
+							isCardRendered
+								? 'favourites__button_type_card_active'
+								: 'favourites__button_type_card'
+						}`}
+						type="button"
+						aria-label="Переключатель отображения площадок в виде карточек"
+						onClick={handleSwitchCardsDisplay}
+					/>
+					<button
+						className={`favourites__button favourites__button_type_list ${
+							!isCardRendered
+								? 'favourites__button_type_list_active'
+								: 'favourites__button_type_list'
+						}`}
+						type="button"
+						aria-label="Переключатель отображения площадок в виде списка"
+						onClick={handleSwitchCardsDisplay}
+					/>
+				</div>
 			</nav>
+			{isCardRendered ? (
+				<article className="favourites__card-list">
+					{favoritesArea.map((area) => (
+						<Card key={area.id} area={area} />
+					))}
+				</article>
+			) : (
+				<article className="favourites__list">
+					{favoritesArea.map((area) => (
+						<Card key={area.id} area={area} />
+					))}
+				</article>
+			)}
 		</section>
 	);
 }
