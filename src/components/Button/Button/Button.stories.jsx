835bd70da31a578f89c420862bd7c4edae349954
RubyVine01 <leraprokofevaiera1@gmail.com>
2024-04-01 20:53:17 +0300

    add button

--- src/components/Button/Button/Button.stories.jsx
+++ src/components/Button/Button/Button.stories.jsx
@@ -1,4 +1,5 @@
-import Search from '../../svg/Search';
+import PhotoImg from '../../svg/PhotoImg';
+import SearchImg from '../../svg/SearchImg';
 import { Button } from './Button';
 
 export default {
@@ -7,27 +8,28 @@ export default {
 	argTypes: {
 		label: { control: 'text' },
 		type: {
-			control: 'select',
+			control: 'radio',
 			options: ['button', 'submit', 'reset'],
 		},
 		btnStyle: {
-			control: 'select',
+			control: 'radio',
 			options: ['primary', 'secondary', 'flat'],
 		},
 		size: {
-			control: 'select',
+			control: 'radio',
 			options: ['big', 'small'],
+		},
+		iconPosition: {
+			control: 'radio',
+			options: ['start', 'end'],
 		},
 		disabled: { control: 'boolean' },
 		onClick: { action: 'clicked' },
+		ariaLabel: { control: 'text' },
 	},
 };
 
-const Template = (args) => (
-	<Button {...args}>
-		<Search />
-	</Button>
-);
+const Template = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
@@ -36,5 +38,40 @@ Default.args = {
 	btnStyle: 'primary',
 	size: 'big',
 	disabled: false,
-	startIcon: true,
+	startIcon: <SearchImg />,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+	label: 'Button',
+	type: 'button',
+	btnStyle: 'secondary',
+	size: 'small',
+	disabled: false,
+	endIcon: <SearchImg />,
+};
+
+export const Flat = Template.bind({});
+Flat.args = {
+	label: 'Button',
+	type: 'button',
+	btnStyle: 'flat',
+	size: 'small',
+	disabled: false,
+	startIcon: <SearchImg />,
+};
+
+export const СustomAddPhoto = Template.bind({});
+СustomAddPhoto.args = {
+	label: 'Добавить фото',
+	disabled: false,
+	customStyle: 'btn-add-photo',
+	startIcon: <PhotoImg />,
+};
+
+export const СustomAddPhotoSmall = Template.bind({});
+СustomAddPhotoSmall.args = {
+	disabled: false,
+	customStyle: 'btn-add-photo-small',
+	startIcon: <PhotoImg />,
 };
